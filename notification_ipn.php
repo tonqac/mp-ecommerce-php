835cfca7349d74b660f8fd570fcac8aaa5f6bf96
Gaston Gimenez <gimenezga@gmail.com>
2020-10-01 13:24:45 -0300

    1.3

--- notification_ipn.php
+++ notification_ipn.php
@@ -1,5 +1,5 @@
 <?php
 	$txt = "\n".date("Y-m-d H:i:s"). "\n". print_r($_REQUEST,true);
 
-	file_put_contents('results.json', $txt, FILE_APPEND | LOCK_EX);
+	file_put_contents('results.txt', $txt, FILE_APPEND | LOCK_EX);
 ?>
