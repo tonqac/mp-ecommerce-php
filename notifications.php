2eae5aaeb6df1b4a27aa720a16fbbde6a11d9530
Gaston Gimenez <gimenezga@gmail.com>
2020-10-02 16:48:19 -0300

    Update notifications.php

--- notifications.php
+++ notifications.php
@@ -1,28 +1,31 @@
 <?php
-    $txt = "\n INIT NOTIFICATION POST \n".date("Y-m-d H:i:s"). "\n". print_r($_POST,true);
+    $txt = "\n INIT NOTIFICATION POST \n".date("Y-m-d H:i:s"). "\n". json_encode($_POST);
     file_put_contents('results.txt', $txt, FILE_APPEND | LOCK_EX);
 
-    $txt = "\n INIT NOTIFICATION GET \n".date("Y-m-d H:i:s"). "\n". print_r($_GET,true);
+    $txt = "\n INIT NOTIFICATION GET \n".date("Y-m-d H:i:s"). "\n". json_encode($_GET);
     file_put_contents('results.txt', $txt, FILE_APPEND | LOCK_EX);
 
-    $txt = "\n INIT NOTIFICATION REQUEST \n".date("Y-m-d H:i:s"). "\n". print_r($_REQUEST,true);
+    $txt = "\n INIT NOTIFICATION REQUEST \n".date("Y-m-d H:i:s"). "\n". json_encode($_REQUEST);
     file_put_contents('results.txt', $txt, FILE_APPEND | LOCK_EX);
 
-    // SDK de Mercado Pago
-    require 'vendor/autoload.php';
+    if($_POST["type"]=="payment"){
+        
+        // SDK de Mercado Pago
+        require 'vendor/autoload.php';
 
-    MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
+        MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 
-    // Obtengo información del pago
-    $payment = MercadoPago\Payment::find_by_id($_GET["data_id"]);
-    $merchant_order = MercadoPago\MerchantOrder::find_by_id($payment->order->id);
+        // Obtengo información del pago
+        $payment = MercadoPago\Payment::find_by_id($_GET["data_id"]);
+        $merchant_order = MercadoPago\MerchantOrder::find_by_id($payment->order->id);
 
-    $txt = "\n GET NOTIFICATION \n".date("Y-m-d H:i:s"). "\n". print_r($merchant_order,true);
-    file_put_contents('results.txt', $txt, FILE_APPEND | LOCK_EX);
+        $txt = "\n GET NOTIFICATION \n".date("Y-m-d H:i:s"). "\n". print_r($merchant_order,true);
+        file_put_contents('results.txt', $txt, FILE_APPEND | LOCK_EX);
 
-    $json = json_encode((array) $merchant_order, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        $json = json_encode((array) $merchant_order, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
 
-    file_put_contents('results.json', $json);
+        file_put_contents('results.json', $json);
+    }
 
     header('Content-Type: application/json');
     echo json_encode(['HTTP/1.1 200 OK'], 200);
